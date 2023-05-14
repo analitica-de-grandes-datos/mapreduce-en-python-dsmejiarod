0d55bdaf7554d7cd306c3e51a21ad99770b8d75e
Daniel Mejia <dsmejiar@unal.edu.co>
2023-05-14 10:37:20 -0500

    intento2

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+for row in sys.stdin:
+  elementos = row.strip()
+  elementos = elementos.split("   ")
+  
+  
+  linea = ";".join(elementos)
+  sys.stdout.write(linea+"\n")
--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+from datetime import date
+
+def prettyformat(x):
+  if x <10:
+    x = "0"+str(x)
+  else:
+    x =  str(x)
+  return x
+
+for row in sys.stdin:
+  elementos = row.strip()
+  elementos = elementos.split("   ")
+  
+  valor = date.fromisoformat(elementos[1])
+  mes = prettyformat(valor.month)
+
+   
+  sys.stdout.write(mes+"\n")
